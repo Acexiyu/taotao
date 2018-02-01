6551c96e68a42a6548d78f1e6d73c58f88647386
Ace <acexiyu@163.com>
2018-02-01 19:38:29 +0800

    初次提交:2018-02-01

---
+++ taotao-manage/taotao-manage-interface/src/main/java/com/taotao/manage/service/TestService.java
@@ -0,0 +1,6 @@
+package com.taotao.manage.service;
+
+public interface TestService {
+
+	public String queryCurrentDate();
+}
---
+++ taotao-manage/taotao-manage-mapper/src/main/java/com/taotao/manage/mapper/TestMapper.java
@@ -0,0 +1,10 @@
+package com.taotao.manage.mapper;
+
+public interface TestMapper {
+
+	/**
+	 * 查询当前时间
+	 * @return
+	 */
+	public String queryCurrentDate();
+}
